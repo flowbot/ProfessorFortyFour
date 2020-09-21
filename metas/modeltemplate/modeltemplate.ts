9fdc700ecf48f56113198a720683fe29764ff77b
flowbot <flowbot@outlook.com>
2020-09-21 12:09:46 -0400

    Minimal viable

--- metas/modeltemplate/modeltemplate.ts
+++ metas/modeltemplate/modeltemplate.ts
@@ -8,6 +8,10 @@ export class ModelTemplate implements ISystem {
   /// --- Lets make a 3D model ---
   modelEntity = null
 
+  // active gardens
+  gardens: Array<string> = ["-104,-85","10,70","-114,-70","-115,-69","119,-20","-125,-67","-133,-68","-138,-124","-140,-50","140,-56","142,-59","-144,-123","144,-53","14,-86","150,4","18,-119","21,-125","2,-135","21,88","24,-137","-24,-23","-24,7","-26,-116","-27,4","28,-119","31,-65","32,67","3,-34","-35,-87","-38,-53","39,-122","-39,31","41,-1","4,-111","43,-1","-44,-110","45,-1","46,-1","-48,-56","-48,-57","49,-6","49,-91","5,-111","-55,-3","56,119","57,28","58,-24","60,-124","6,-111","62,29","-62,45","6,-64","-75,-63","88,29","90,135","-91,-91"]
+  //gardens: Array<string> = ["49,-91"]
+
   /**
    * Initial scene setup, create all objects in the constructor.
    *
@@ -25,10 +29,21 @@ export class ModelTemplate implements ISystem {
 
     /// --- Lets spawn a 3d model ---
     this.modelEntity = new Entity()
-    this.modelEntity.addComponent(new GLTFShape('metas/modeltemplate/models/chair.glb'))
+    this.modelEntity.addComponent(new GLTFShape('metas/modeltemplate/models/alice.glb'))
     this.modelEntity.addComponent(new Transform({
       position: new Vector3(0, 2, 0)
     }))
+    this.modelEntity.addComponent(
+      new OnPointerDown(
+        e =>{
+          teleportTo(this.randonGarden())
+        },
+        {
+          button: ActionButton.POINTER,
+          hoverText: 'Teleport to Another Garden'
+        }
+      )
+    );
     engine.addEntity(this.modelEntity)
 
     ///////// Your static scene assets ///////////
@@ -38,6 +53,12 @@ export class ModelTemplate implements ISystem {
     // Initial host data
     this.refreshHost(host_data)
   }
+
+    //get a randome garden plot
+    randonGarden() {
+      // "|" for a kinda "int div"
+      return this.gardens[this.gardens.length * Math.random() | 0];
+    }
 
   /**
    * A Decentraland provided function where you should put your code that
