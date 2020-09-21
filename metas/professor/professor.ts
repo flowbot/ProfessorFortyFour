6911bd2cb1b4f550f2573a4f0a9dfb4ed6ed5be8
flowbot <flowbot@outlook.com>
2020-09-21 17:56:14 -0400

    Inital dialog for NPC set with random Teleport to static gardens

--- metas/professor/professor.ts
+++ metas/professor/professor.ts
@@ -1,5 +1,13 @@
+import * as ui from '../../node_modules/@dcl/ui-utils/index'
+import { NPCTalk } from "./dialogData"
 
 export class Professor implements ISystem {
+  // active gardens
+  static gardens: Array<string> = ["-104,-85","10,70","-114,-70","-115,-69","119,-20","-125,-67","-133,-68","-138,-124","-140,-50","140,-56","142,-59","-144,-123","144,-53","14,-86","150,4","18,-119","21,-125","2,-135","21,88","24,-137","-24,-23","-24,7","-26,-116","-27,4","28,-119","31,-65","32,67","3,-34","-35,-87","-38,-53","39,-122","-39,31","41,-1","4,-111","43,-1","-44,-110","45,-1","46,-1","-48,-56","-48,-57","49,-6","49,-91","5,-111","-55,-3","56,119","57,28","58,-24","60,-124","6,-111","62,29","-62,45","6,-64","-75,-63","88,29","90,135","-91,-91"]
+  static randonGardenTeleport() {
+    teleportTo(this.gardens[this.gardens.length * Math.random() | 0]) 
+  }
+
   META_ID = 0 // Change to your MetaZone meta number identifier
 
   api = null
@@ -7,10 +15,6 @@ export class Professor implements ISystem {
 
   /// --- Lets make a 3D model ---
   professorModel = null
-
-  // active gardens
-  gardens: Array<string> = ["-104,-85","10,70","-114,-70","-115,-69","119,-20","-125,-67","-133,-68","-138,-124","-140,-50","140,-56","142,-59","-144,-123","144,-53","14,-86","150,4","18,-119","21,-125","2,-135","21,88","24,-137","-24,-23","-24,7","-26,-116","-27,4","28,-119","31,-65","32,67","3,-34","-35,-87","-38,-53","39,-122","-39,31","41,-1","4,-111","43,-1","-44,-110","45,-1","46,-1","-48,-56","-48,-57","49,-6","49,-91","5,-111","-55,-3","56,119","57,28","58,-24","60,-124","6,-111","62,29","-62,45","6,-64","-75,-63","88,29","90,135","-91,-91"]
-  //gardens: Array<string> = ["49,-91"]
 
   /**
    * Initial scene setup, create all objects in the constructor.
@@ -22,7 +26,7 @@ export class Professor implements ISystem {
     // Save api
     this.api = api;
 
-
+    let dialogWindow = new ui.DialogWindow()
 
     ///////// Your static scene assets ///////////
     // Initialize all scene entities here
@@ -31,34 +35,37 @@ export class Professor implements ISystem {
     this.professorModel = new Entity()
     this.professorModel.addComponent(new GLTFShape('metas/professor/models/alice.glb'))
     this.professorModel.addComponent(new Transform({
-      position: new Vector3(0, 2, 0)
+      position: new Vector3(8, 1, 8)
     }))
     this.professorModel.addComponent(
       new OnPointerDown(
         e =>{
-          teleportTo(this.randonGarden())
+          //teleportTo(this.randonGarden())  
+          if (!dialogWindow.isDialogOpen){
+            dialogWindow.openDialogWindow(NPCTalk, 0)
+          }
         },
         {
           button: ActionButton.POINTER,
-          hoverText: 'Teleport to Another Garden'
+          hoverText: 'Talk'
         }
       )
-    );
+    )
+    const ringsModel = new Entity()
+    ringsModel.addComponent(new GLTFShape('metas/professor/models/rings.glb'))
+    ringsModel.addComponent(
+      new Transform({
+        position: new Vector3(0, -0.65, 0),
+      })
+    )
+    ringsModel.setParent(this.professorModel)
     engine.addEntity(this.professorModel)
 
     ///////// Your static scene assets ///////////
 
-
-
     // Initial host data
     this.refreshHost(host_data)
   }
-
-    //get a randome garden plot
-    randonGarden() {
-      // "|" for a kinda "int div"
-      return this.gardens[this.gardens.length * Math.random() | 0];
-    }
 
   /**
    * A Decentraland provided function where you should put your code that
