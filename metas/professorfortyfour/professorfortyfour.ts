55f7105ade57f32c81d22d55735eb69853335536
Oscar Gomez <corvstudios@gmail.com>
2020-09-22 22:06:06 -0500

    Moved list of gardens to meta data

--- metas/professorfortyfour/professorfortyfour.ts
+++ metas/professorfortyfour/professorfortyfour.ts
@@ -4,10 +4,35 @@ import { Dialog } from '../../node_modules/@dcl/ui-utils/utils/types'
 export class Professor implements ISystem {
   // active gardens
   //todo get from an api
-  static gardens: Array<string> = ["-104,-85","10,70","-114,-70","-115,-69","119,-20","-125,-67","-133,-68","-138,-124","-140,-50","140,-56","142,-59","-144,-123","144,-53","14,-86","150,4","18,-119","21,-125","2,-135","21,88","24,-137","-24,-23","-24,7","-26,-116","-27,4","28,-119","31,-65","32,67","3,-34","-35,-87","-38,-53","39,-122","-39,31","41,-1","4,-111","43,-1","-44,-110","45,-1","46,-1","-48,-56","-48,-57","49,-6","49,-91","5,-111","-55,-3","56,119","57,28","58,-24","60,-124","6,-111","62,29","-62,45","6,-64","-75,-63","88,29","90,135","-91,-91"]
-  static randonGardenTeleport() {
-    teleportTo(this.gardens[this.gardens.length * Math.random() | 0]) 
+  gardens: Array<string> = []
+  randonGardenTeleport() {
+    teleportTo(this.gardens[this.gardens.length * Math.random() | 0])
   }
+
+  NPCTalk: Dialog[] = [
+    {
+      text: 'Hi there, I am Professor Forty Four! I am here to help you navigate Ethermon location in Decentraland',
+    },
+    {
+      text: 'Would you like me to teleport you now to an Ethermon Garden location?',
+      isQuestion: true,
+      labelE: { label: "YES", fontSize: 14 },
+      triggeredByE: () => {
+        this.randonGardenTeleport()
+      },
+      ifPressE: 3,
+      labelF: { label: "NO", fontSize: 14 },
+      ifPressF: 2,
+    },
+    {
+      text: 'Welp, that is all I can do right now talk to you later',
+      isEndOfDialog: true,
+    },
+    {
+      text: 'bloop bloop bloop...oh you decided not to go, ok just let me know when you are ready',
+      isEndOfDialog: true,
+    },
+  ]
 
   META_ID = 270 // Change to your MetaZone meta number identifier
 
@@ -42,7 +67,7 @@ export class Professor implements ISystem {
       new OnPointerDown(
         e =>{
           if (!dialogWindow.isDialogOpen){
-            dialogWindow.openDialogWindow(NPCTalk, 0)
+            dialogWindow.openDialogWindow(this.NPCTalk, 0)
           }
         },
         {
@@ -98,6 +123,11 @@ export class Professor implements ISystem {
     // Save host info
     this.host = host
 
+    if(this.host.meta_data) {
+      let meta_data = JSON.parse(this.host.meta_data)
+      this.gardens = meta_data.gardens;
+    }
+
     // Parse metadata
     if(this.host.host_data) {
       let host_data = JSON.parse(this.host.host_data)
@@ -107,19 +137,19 @@ export class Professor implements ISystem {
 
       /// --- Lets adjust our 3d model ---
       this.professorModel.getComponent(Transform).position.set(
-        host_data.meta.position.x,
-        host_data.meta.position.y,
-        host_data.meta.position.z
+        host_data.professor.position.x,
+        host_data.professor.position.y,
+        host_data.professor.position.z
       )
       this.professorModel.getComponent(Transform).rotation.setEuler(
-        host_data.meta.rotation.x,
-        host_data.meta.rotation.y,
-        host_data.meta.rotation.z
+        host_data.professor.rotation.x,
+        host_data.professor.rotation.y,
+        host_data.professor.rotation.z
       )
       this.professorModel.getComponent(Transform).scale.set(
-        host_data.meta.scale.x,
-        host_data.meta.scale.y,
-        host_data.meta.scale.z
+        host_data.professor.scale.x,
+        host_data.professor.scale.y,
+        host_data.professor.scale.z
       )
 
       ///////// Your landowner adjustable content ///////////
@@ -146,7 +176,7 @@ export function addFaceUserSystem(dummyTarget: Entity) {
       for (let robot of this.robotGroup.entities) {
         let transform = robot.getComponent(Transform)
         let trackUserSlerp = robot.getComponent(TrackUserSlerp)
-        
+
         // Check if player moves
         if(!currentCameraPosition.equals(Camera.instance.position)) {
           // Update current camera position
@@ -171,28 +201,3 @@ export function addFaceUserSystem(dummyTarget: Entity) {
 
   engine.addSystem(new FaceUserSystem())
 }
-
-let NPCTalk: Dialog[] = [
-  {
-    text: 'Hi there, I am Professor Forty Four! I am here to help you navigate Ethermon location in Decentraland',
-  },
-  {
-    text: 'Would you like me to teleport you now to an Ethermon Garden location?',
-    isQuestion: true,
-    labelE: { label: "YES", fontSize: 14 },
-    triggeredByE: () => {
-      Professor.randonGardenTeleport()
-    },
-    ifPressE: 3,
-    labelF: { label: "NO", fontSize: 14 },
-    ifPressF: 2,
-  },
-  {
-    text: 'Welp, that is all I can do right now talk to you later',
-    isEndOfDialog: true,
-  },
-  {
-    text: 'bloop bloop bloop...oh you decided not to go, ok just let me know when you are ready',
-    isEndOfDialog: true,
-  },
-]
