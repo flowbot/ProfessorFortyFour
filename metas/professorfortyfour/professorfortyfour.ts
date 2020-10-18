4bfff34faaa1104464b68230b63b2c0584ab913f
flowbot <flowbot@outlook.com>
2020-10-18 08:52:52 -0400

    added backup data if game.ts data was not found

--- metas/professorfortyfour/professorfortyfour.ts
+++ metas/professorfortyfour/professorfortyfour.ts
@@ -150,8 +150,21 @@ export class ProfessorFortyFour implements ISystem {
 
     if(this.host.meta_data) {
       let meta_data = JSON.parse(this.host.meta_data)
-      this.gardens = meta_data.gardens
-      this.nests = meta_data.nests
+      if(meta_data.nests){
+        //log("garden meta found")
+        this.gardens = meta_data.gardens
+      }else{
+        //log("going to the backup")
+        this.gardens = ["10,70","-115,-69","119,-20","-125,-67","-133,-68","-138,-124","-140,-50","140,-56","142,-59","-144,-123","144,-53","14,-86","21,-125","2,-135","21,88","24,-137","-24,-23","-24,7","28,-119","31,-65","3,-34","-35,-87","-38,-53","-39,31","4,-111","-44,-110","-48,-56","-48,-57","49,-91","5,-111","-55,-3","56,119","57,28","58,-24","60,-124","6,-111","62,29","-62,45","6,-64","-75,-63","88,29","-91,-91"]
+      }
+      if(meta_data.nests){
+        //log("nest meta found")
+        this.nests = meta_data.nests
+      }else{
+        //log("going to the backup")
+        this.nests = ["2,-119", "49,-91", "28,-119", "3,-34", "-48,-57", "-92,-88"]
+      }
+
     }
 
     // Parse metadata
